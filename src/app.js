94d1a02f413353ffdce4feb8c5d007a2b5be0603
bd2187 <brandon.dionisio.26@gmail.com>
2020-03-03 19:25:02 -0800

    Set up ESLint and Prettier configurations

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
-import "./styles/normalize.css";
+import React from 'react';
+import { render } from 'react-dom';
+import './styles/normalize.css';
 
 const App = function() {
-    return <h1>hello world</h1>;
+  return <h1>hello world</h1>;
 };
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
