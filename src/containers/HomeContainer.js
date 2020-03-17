d721356ba9a9ea6f0697e5dea33d5508122aa860
bd2187 <brandon.dionisio.26@gmail.com>
2020-03-16 21:37:39 -0700

    Initial commit. Create HomeContainer

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Home from '../components/Home/Home';
+
+function HomeContainer() {
+    return <Home />;
+}
+
+export default HomeContainer;
