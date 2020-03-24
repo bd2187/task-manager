32f0a477969762a6251cfbdc2a79388df00ac279
bd2187 <brandon.dionisio.26@gmail.com>
2020-03-23 21:14:23 -0700

    Style icons and implement primary and secondary colors

--- src/components/GuestLanding/GuestLandingStyles.js
+++ src/components/GuestLanding/GuestLandingStyles.js
@@ -1,37 +1,62 @@
 import styled from 'styled-components';
 
+const primaryColor = '#64abe8';
+const secondaryColor = '#333';
+
 const GuestLandingStyles = styled.div`
-    /* background-color: red; */
     min-height: 100vh;
     min-width: 100vw;
     padding: 10px;
+    color: ${secondaryColor};
+
+    a {
+        color: ${primaryColor};
+        text-decoration: underline;
+    }
 
     .logo {
         font-size: 20px;
+        color: ${primaryColor};
     }
 
     .login-btn,
-    .login-inner__input {
+    .login-inner__input,
+    .login-inner__login-btn {
         display: block;
         width: 100%;
-        background: yellow;
+        background: #fff;
         padding: 10px;
         font-size: 14px;
-        border: 1px solid grey;
+        border: 1px solid #ddd;
         border-radius: 4px;
     }
 
     .login-btn {
         text-align: center;
         margin: 13px auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .fa-google {
+            color: #db3236;
+        }
+
+        .fa-facebook-square {
+            color: #3b5998;
+        }
+
+        .login-btn__logo {
+            margin: 0 10px;
+            font-size: 16px;
+        }
     }
 
-    .login-inner__input {
+    .login-inner__login-btn {
         margin: 8px 0px 25px;
     }
 
     .login-inner {
-        /* background-color: blue; */
         margin: 15px auto;
 
         .login-inner__title {
@@ -47,8 +72,13 @@ const GuestLandingStyles = styled.div`
         }
     }
 
+    .login-inner__login-btn {
+        background: ${primaryColor};
+        color: #fff;
+    }
+
     .divider {
-        background: orange;
+        background: #ddd;
         position: relative;
         width: 100%;
         height: 1px;
@@ -70,6 +100,7 @@ const GuestLandingStyles = styled.div`
         width: max-content;
         align-items: center;
         margin-bottom: 25px;
+        cursor: pointer;
     }
 
     .keep-login #keepLoggedIn {
