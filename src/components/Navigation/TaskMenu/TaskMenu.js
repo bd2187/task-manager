e59f99f7f98653afd7240e89e48d74043b8ec04b
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-03 15:27:40 -0700

    add markup for tasks menu

--- src/components/Navigation/TaskMenu/TaskMenu.js
+++ src/components/Navigation/TaskMenu/TaskMenu.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import TaskMenuStyles from './TaskMenuStyles';
+import { Link } from 'react-router-dom';
 
 const TaskMenu = function TaskMeu() {
     return (
         <TaskMenuStyles className="task-menu">
-            <h1>test</h1>
+            <ul className="task-menu__list">
+                <li>
+                    <i className="fas fa-calendar-times" />
+                    <Link to="/dashboard/today">Today</Link>
+                </li>
+                <li>
+                    <i className="fas fa-calendar-alt" />
+                    <Link to="/dashboard/next-7-days">Next 7 Days</Link>
+                </li>
+            </ul>
+            <ul className="task-menu__expansions">
+                <li>
+                    <p>Projects</p>
+                </li>
+                <li>
+                    <p>Labels</p>
+                </li>
+                <li>
+                    <p>Filters</p>
+                </li>
+            </ul>
         </TaskMenuStyles>
     );
 };
