0ca1b4b0149ca98fe1e508ec42cf37f13f57db27
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-03 13:41:26 -0700

    initial commit. create NavigationContainer

---
+++ src/containers/NavigationContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Navigation from '../components/Navigation/Navigation';
+
+function NavigationContainer() {
+    return <Navigation />;
+}
+
+export default NavigationContainer;
