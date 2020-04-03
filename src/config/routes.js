25335e7ee0d16ba780d054a068a9b719ed1bc482
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-03 13:43:35 -0700

    Add dashboard route

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import HomeContainer from '../containers/HomeContainer';
+import DashboardContainer from '../containers/DashboardContainer';
 
 const routes = (
     <Router>
         <Switch>
-            <Route path="/" component={HomeContainer} />
+            <Route exact path="/" component={HomeContainer} />
+            <Route exact path="/dashboard" component={DashboardContainer} />
         </Switch>
     </Router>
 );
